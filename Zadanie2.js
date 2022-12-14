afc7ef742f74c8378dc7491ae2d0b242d8d837ff
Alexandr Lagutkin <116781254+SanCreator@users.noreply.github.com>
2022-12-14 12:42:03 +0700

    Add files via upload

---
+++ Zadanie2.js
@@ -0,0 +1,28 @@
+let arr = [
+    { label: 'Василий' },
+    { label: 'Иван' },
+    { label: 'Пётр' },
+]
+
+function createSelect(arr, str) {
+    let select = document.createElement("select");
+    if (Array.isArray(arr)) {
+        if (typeof arr[0] === "string")
+            for (let value of arr) {
+                let isSelected = str === value;
+                let option = new Option(value, value, isSelected, isSelected);
+                select.options.add(option);
+            }
+        else
+            for (let e of arr) {
+                let value = e.value;
+                let isSelected = str === value;
+                let option = new Option(e.label, value, isSelected, isSelected);
+                select.options.add(option);
+            }
+    
+    }
+    return select;
+}
+
+document.body.append(createSelect(arr));
